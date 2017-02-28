f35693d47ed48ea97eab86a318bdd960796cd584
Neil Okamoto <neil.okamoto@gmail.com>
2017-02-27 23:54:40 -0800

    deploy to clojars on successful build

--- project.clj
+++ project.clj
@@ -6,6 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.hawkular.apm/hawkular-apm-client-opentracing "0.14.1.Final"]
                  [org.hawkular.apm/hawkular-apm-trace-publisher-rest-client "0.14.1.Final"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-cloverage "1.0.9"]]}})
